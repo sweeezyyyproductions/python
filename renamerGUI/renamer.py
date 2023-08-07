7e2f71b134ca7f0c8c0d911e28c6041e289581b9
sweeezyyyproductions <sweeezyyyproductions@gmail.com>
2023-08-06 23:07:38 -0700

    created GUI, example of 1st filename in directory is given

--- renamerGUI/renamer.py
+++ renamerGUI/renamer.py
@@ -14,6 +14,17 @@ def browse_directory():
     directory_path = filedialog.askdirectory()
     entry_directory.delete(0, tk.END)
     entry_directory.insert(0, directory_path)
+    update_example()  # Update the example label after selecting the directory
+
+def update_example():
+    directory_path = entry_directory.get()
+    prefix = entry_prefix.get()
+
+    # Fetch the first filename in the directory (if available)
+    files = os.listdir(directory_path)
+    first_filename = files[0] if files else ""
+
+    example_label.config(text=f"Example: {prefix}{first_filename}")
 
 def run_script():
     directory_path = entry_directory.get()
@@ -31,8 +42,12 @@ button_browse = tk.Button(main, text="Browse", command=browse_directory)
 
 label_prefix = tk.Label(main, text="Enter Prefix:")
 entry_prefix = tk.Entry(main, width=50)
+entry_prefix.bind("<KeyRelease>", lambda event: update_example())  # Bind the key release event to update_example()
 
 button_run = tk.Button(main, text="Run Script", command=run_script)
+
+# Create a Label widget to display an example of the renamed files
+example_label = tk.Label(main, text="Example: ", fg="blue")
 
 # Layout the widgets using the grid geometry manager
 label_directory.grid(row=0, column=0, padx=10, pady=5)
@@ -41,8 +56,9 @@ button_browse.grid(row=0, column=2, padx=10, pady=5)
 
 label_prefix.grid(row=1, column=0, padx=10, pady=5)
 entry_prefix.grid(row=1, column=1, padx=10, pady=5)
+example_label.grid(row=2, column=0, columnspan=3, padx=10, pady=5)
 
-button_run.grid(row=2, column=0, columnspan=3, padx=10, pady=5)
+button_run.grid(row=3, column=0, columnspan=3, padx=10, pady=5)
 
 # Start the main event loop
 main.mainloop()
